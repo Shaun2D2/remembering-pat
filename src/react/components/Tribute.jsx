accbe6a2d3d30bc3f1e0e4f10036958dd600cc6f
Shaun Lobsinger <shaun@jolobo.com>
2021-02-04 00:08:47 -0700

    fixed styles for tribute video

--- src/react/components/Tribute.jsx
+++ src/react/components/Tribute.jsx
@@ -5,7 +5,7 @@ import Section from './Section';
 const Tribute = () => (
   <Section title="Tribute" id="tribute">
     <div className="row">
-      <div className="col-sm-8 offset-2">
+      <div className="col-sm-12 col-md-8 offset-md-2">
         <iframe width="100%" height="315" src="https://www.youtube.com/embed/2Nygap-HB2s" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
 
         <a href="https://www.youtube.com/watch?v=2Nygap-HB2s&ab_channel=JimDiBattista" target="_blank" rel="noreferrer noopener">see on Youtube</a>
