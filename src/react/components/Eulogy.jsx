d73b191de58318e9fde10b84835d5071956e5552
Shaun Lobsinger <shaun@jolobo.com>
2021-02-14 18:54:00 -0700

    tried to fix background stuff, also added poem.

--- src/react/components/Eulogy.jsx
+++ src/react/components/Eulogy.jsx
@@ -13,8 +13,37 @@ const Eulogy = () => (
         <Img src="./pat-1.jpg" className="eulogy-image" alt="patrick lobsinger" />
       </div>
       <div className="col-sm-8">
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque bibendum, eros nec euismod porttitor, metus velit congue diam, in rhoncus sapien erat quis augue. Donec non enim sed velit ornare auctor vel non sapien. Pellentesque maximus sapien ut enim dignissim, ut consectetur dolor consequat. Sed accumsan dapibus enim, non dictum nisi ullamcorper eu. Nulla viverra nisi non porta lobortis. Cras lorem massa, condimentum ut lacinia id, condimentum ac mauris. Morbi sit amet arcu porta, molestie tellus non, iaculis est. Morbi in justo libero. Nunc et sollicitudin nisl.</p>
-        <p>Nam eget nisl id orci accumsan pharetra. Duis hendrerit tristique turpis vel ullamcorper. Suspendisse potenti. Donec in quam porttitor, varius ipsum sed, condimentum eros. Aenean elit urna, sagittis ut magna ac, pretium mollis velit. Ut sagittis, velit in posuere malesuada, est mauris hendrerit purus, ac placerat libero diam in justo. Nunc pretium eget ex tempus laoreet. Phasellus porta arcu ut erat accumsan, in finibus nibh ultricies. Etiam libero libero, fringilla eget volutpat eu, sagittis ac felis. Nam pharetra quam eu hendrerit pretium. Integer hendrerit ultricies mi et egestas. Curabitur sit amet dui nec lectus tempor placerat lobortis at magna. Sed id mauris eu ante sollicitudin fringilla sed a diam. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+        <p className="poem">
+          <div className="poem__title">I Will Sleep</div>
+          <br />
+          They say I've been born
+          <br />
+          I've been handed hand to hand
+          <br />
+          I will sleep and you will watch me grow
+          <br />
+          Into the years, everyone will know
+          <br />
+          I will be, the next one to get old
+          <br />
+          I only stand when I'm in someone's hands
+          <br />
+          I don't know where I am
+          <br />
+          I will sleep and you will watch me grow
+          <br />
+          Don't close your eyes, or you will miss my show
+          <br />
+          For now I'll smile, cuz I can't say hello
+          <br />
+          I will sleep and you will watch me grow
+          <br />
+          I'll never have it so good.
+        </p>
+        <p className="poem__attribution">
+          - Patrick Lobsinger (6/28/1981)
+        </p>
+
       </div>
     </div>
   </Section>
